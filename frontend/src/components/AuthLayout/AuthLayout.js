21924f2ecdae300a15e69e977e6db7c171d2014a
koranat <47288.koranat@gmail.com>
2022-11-12 22:44:38 +0700

    :art: Edit auth layout CSS

--- frontend/src/components/AuthLayout/AuthLayout.js
+++ frontend/src/components/AuthLayout/AuthLayout.js
@@ -2,15 +2,18 @@ import styles from '../../assets/css/AuthLayout.module.css'
 
 const AuthLayout = ({ children, image, pageTitle }) => {
 	return (
-		<div
-			className={styles.layout}
-			style={{ backgroundImage: `url(${image})` }}
-		>
+		<div className={styles.layout}>
+			<div
+				className={styles.banner}
+				style={{ backgroundImage: `url(${image})` }}
+			></div>
 			<div className={styles.authCard}>
-				<h1 className={styles.applicationName}>An sample app</h1>
-				<hr />
-				<h3 className={styles.pageTitle}>{pageTitle}</h3>
-				{children}
+				<div className={styles.cardContent}>
+					<h1 className={styles.applicationName}>An sample app</h1>
+					<hr />
+					<h3 className={styles.pageTitle}>{pageTitle}</h3>
+					{children}
+				</div>
 			</div>
 		</div>
 	)
