8f69bb2be27fbb1d071aadaf3ab3064f02969253
koranat <47288.koranat@gmail.com>
2022-11-12 23:45:41 +0700

    :sparkles: Add profile page UI

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,5 +1,33 @@
+import Button from '../components/Button'
+
+import styles from '../assets/css/ProfilePage.module.css'
+import ProfileImage from '../assets/img/profile.png'
+import QRCode from '../assets/img/qrcode1.png'
+
 const ProfilePage = () => {
-	return <></>
+	return (
+		<div className={styles.container}>
+			<nav className={styles.navbar}>
+				<div className={styles.navbarContainer}>
+					<img
+						className={styles.profileImage}
+						src={ProfileImage}
+						alt="profile-img"
+					/>
+					<h2 className={styles.username}>John Appleseed</h2>
+					<h3 className={styles.email}>john@example.com</h3>
+				</div>
+			</nav>
+			<main className={styles.content}>
+				<img src={QRCode} alt="qrcode" />
+				<p>
+					tpauth://totp/BSthun%20KB%20BookStack%3Aco%40bsthun.com?
+					secret=L6UVQ7F5O4BWDYHV
+				</p>
+				<Button>Regenerate</Button>
+			</main>
+		</div>
+	)
 }
 
 export default ProfilePage
