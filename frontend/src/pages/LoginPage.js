2f74c2f565ec6c43a29758646b6b33efa9a39e64
Sirawit Pratoomsuwan <s36526@bj.ac.th>
2022-11-19 00:24:07 +0700

    feat: success yay!

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,98 @@
+import AuthLayout from '../components/AuthLayout'
+import Button from '../components/Button'
+import Link from '../components/Link/Link'
+
+import StarCatcher from '../assets/img/StarCatcher.jpg'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+
+const LoginPage = () => {
+	const navigate = useNavigate()
+
+	function onLoginSubmit(e) {
+		e.preventDefault()
+
+		/** Data */
+		const data = {
+			email: e.target.email.value,
+			password: e.target.password.value,
+		}
+
+		/** Login */
+		axios
+			.post('https://gdsc.sit.kmutt.ac.th/ev1/signin.json', data)
+			.then((res) => {
+				/** If login success */
+				if (res.data?.success) {
+					toast('🦄 Signed in success!', {
+						position: 'top-right',
+						autoClose: 5000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: 'light',
+					})
+
+					/** Navigate to confirmation page. */
+					navigate('/login/confirm', {
+						state: { email: data.email, id: res.data?.id },
+					})
+				} else {
+					toast.error('🦄 Signed in failed!', {
+						position: 'top-right',
+						autoClose: 5000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: 'light',
+					})
+				}
+			})
+			.catch((err) => {
+				toast.error('Signed in failed!', {
+					position: 'top-right',
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: 'light',
+				})
+			})
+	}
+
+	return (
+		<AuthLayout image={StarCatcher} pageTitle="Sign In">
+			<form className="form-container" onSubmit={onLoginSubmit}>
+				<input
+					className="input"
+					name="email"
+					placeholder="Email"
+					type="email"
+					required
+				/>
+				<input
+					className="input"
+					name="password"
+					placeholder="Password"
+					type="password"
+					required
+				/>
+
+				<div className="action-btn-container">
+					<Link to="/register">Register</Link>
+					<Button type="submit">Login</Button>
+				</div>
+			</form>
+			<ToastContainer />
+		</AuthLayout>
+	)
+}
+
+export default LoginPage
---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,141 @@
+import AuthLayout from '../components/AuthLayout'
+import Button from '../components/Button'
+import Link from '../components/Link/Link'
+import Onshore from '../assets/img/Onshore.jpg'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+
+const RegisterPage = () => {
+	const navigate = useNavigate()
+	const [query] = useSearchParams()
+	const dialog = 'http://localhost:3000/login-confirm'
+
+	function onContinue() {
+		navigate('/login')
+	}
+
+	function onRegisterSubmit(e) {
+		e.preventDefault()
+		const data = {
+			email: e.target.email.value.trim(),
+			password: e.target.password.value.trim(),
+			confirm: e.target.confirm.value.trim(),
+		}
+
+		if (data.password !== '' && data.password !== data.confirm) {
+			toast.error('Password not match!', {
+				position: 'top-right',
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: 'light',
+			})
+			return
+		}
+
+		/** Register */
+		axios
+			.post('http://gdsc-int.sit.kmutt.ac.th/ev1/signup.json', data)
+			.then((res) => {
+				/** If register success */
+				if (res.data?.success) {
+					toast('🦄 Signing up success!', {
+						position: 'top-right',
+						autoClose: 5000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: 'light',
+					})
+
+					/** Navigate to confirmation page. */
+					navigate('/register?verify=true', {
+						replace: true,
+					})
+				} else {
+					toast.error('Signing up failed!', {
+						position: 'top-right',
+						autoClose: 5000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: 'light',
+					})
+				}
+			})
+			.catch((err) => {
+				toast.error('Signing up failed!', {
+					position: 'top-right',
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: 'light',
+				})
+			})
+	}
+
+	return (
+		<AuthLayout
+			image={Onshore}
+			pageTitle={query.get('verify') ? 'TOTP Scan' : 'Registration'}
+		>
+			{query.get('verify') ? (
+				<div className="qrcode-container">
+					<img
+						className="qrcode"
+						src={`https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=${dialog}`}
+						alt="profile-img"
+					/>
+					<p>
+						Please scan the QR code with your authenticator. <br />
+						Press 'Continue' once you have scanned the QR code.
+					</p>
+					<Button onClick={onContinue}>Continue</Button>
+				</div>
+			) : (
+				<form className="form-container" onSubmit={onRegisterSubmit}>
+					<input
+						className="input"
+						name="email"
+						placeholder="Email"
+						type="email"
+						required
+					/>
+					<input
+						className="input"
+						name="password"
+						placeholder="Password"
+						type="password"
+						required
+					/>
+					<input
+						className="input"
+						name="confirm"
+						placeholder="Confirm password"
+						type="password"
+						required
+					/>
+
+					<div className="action-btn-container">
+						<Link to="/login">Login</Link>
+						<Button type="submit">Register</Button>
+					</div>
+				</form>
+			)}
+			<ToastContainer />
+		</AuthLayout>
+	)
+}
+
+export default RegisterPage
