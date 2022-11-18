7984a561ba544208a44996710f3b370d03a2ecaf
Floaty_KT <FloatKasemtan@users.noreply.github.com>
2022-11-18 19:19:56 +0700

    feat: turn to template code

--- backend/main.go
+++ backend/main.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"database/sql"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"image/png"
 	"net/http"
 	"time"
 
 	"github.com/GDSC-KMUTT/totp-session/config"
-	"github.com/GDSC-KMUTT/totp-session/handler"
-	"github.com/GDSC-KMUTT/totp-session/repository"
-	"github.com/GDSC-KMUTT/totp-session/service"
-	"github.com/GDSC-KMUTT/totp-session/types"
-	"github.com/GDSC-KMUTT/totp-session/utils"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/golang-jwt/jwt"
-	"github.com/pquerna/otp/totp"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
@@ -34,98 +21,18 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	userRepository := repository.NewRepositoryDB(db)
-	userService := service.NewUserService(userRepository)
-	userHandler := handler.NewUserHandler(userService)
 
-	http.HandleFunc("/", (func(w http.ResponseWriter, r *http.Request) {
-		// Check if the request method is POST
-		if r.Method != "POST" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
+	http.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
+		// implement me!
+	})
 
-		// Set the response header to application/json
-		w.Header().Set("Content-Type", "application/json")
+	http.HandleFunc("/signin", func(w http.ResponseWriter, r *http.Request) {
+		// implement me!
+	})
 
-		// Declare a variable to store the body of the request
-		var response []byte
-		var body types.SignIn
-		err := utils.Parse(r, &body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		// Generate a new secret TOTP key
-		key, err := totp.Generate(totp.GenerateOpts{
-			Issuer:      "GDSC KMUTT",
-			AccountName: body.Email,
-		})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		secret := key.Secret()
-
-		// Hash the password
-		hashedPwd, err := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Create a new user
-		insert, err := db.Exec("INSERT INTO users (email, password, secret) VALUES (?, ?, ?)", body.Email, hashedPwd, secret)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		userId, err := insert.LastInsertId()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Create a new JWT claims
-		claims := jwt.MapClaims{
-			"id":  userId,
-			"exp": time.Now().Add(time.Hour * 72).Unix(),
-		}
-
-		// Create JWT token
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-		tokenString, err := token.SignedString([]byte(config.C.JWT_SECRET))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Convert TOTP key into a PNG, and encode it to base64
-		var buf bytes.Buffer
-		img, err := key.Image(200, 200)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		if err := png.Encode(&buf, img); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		base64string := "data:image/png;base64," + base64.StdEncoding.EncodeToString(buf.Bytes())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Create a response
-		response, _ = json.Marshal(map[string]any{"success": true, "token": tokenString, "image": base64string})
-		fmt.Fprint(w, response)
-	}))
-
-	http.HandleFunc("/signup", userHandler.SignUp)
-	http.HandleFunc("/signin", userHandler.SignIn)
-	http.HandleFunc("/list", userHandler.ListUsers)
+	http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		// implement me!
+	})
 
 	if err := s.ListenAndServe(); err != nil {
 		panic(err)
